435f3b4b5c44804252425dc3566bd21ace66a26d
Senjienji <83118934+Senjienji@users.noreply.github.com>
2022-11-18 15:36:11 +0800

    Create moderation.py

---
+++ cogs/moderation.py
@@ -0,0 +1,81 @@
+import discord
+from discord.ext import commands
+#from replit import db
+import datetime
+
+'''
+if 'Prefix' not in db:
+    db['Prefix'] = {} #{'guild.id': 'prefix'}
+if 'Enabled' not in db:
+    db['Enabled'] = {} #{'guild.id': {'command': enabled}}
+'''
+
+class Moderation(commands.Cog):
+    @commands.command()
+    @commands.has_guild_permissions(kick_members = True)
+    @commands.bot_has_guild_permissions(kick_members = True)
+    async def kick(self, ctx, member: discord.Member, *, reason = 'no reason'):
+        await ctx.guild.kick(member, reason = f'By {ctx.author} for {reason}.')
+        await ctx.reply(f'`{member}` kicked for {reason}.')
+    
+    @commands.command()
+    @commands.has_guild_permissions(ban_members = True)
+    @commands.bot_has_guild_permissions(ban_members = True)
+    async def ban(self, ctx, user: discord.User, *, reason = 'no reason'):
+        await ctx.guild.ban(user, reason = f'By {ctx.author} for {reason}', delete_message_days = 0)
+        await ctx.reply(f'`{user}` banned for {reason}.')
+    
+    @commands.command()
+    @commands.has_guild_permissions(ban_members = True)
+    @commands.bot_has_guild_permissions(ban_members = True)
+    async def unban(self, ctx, *, user: discord.User):
+        await ctx.guild.unban(user)
+        await ctx.reply(f'`{user}` unbanned.')
+    
+    '''
+    @commands.command()
+    @commands.has_guild_permissions(manage_guild = True)
+    async def enable(self, ctx, command):
+        if command.lower() in (command.name for command in ctx.bot.commands):
+            if command.lower() in ('enable', 'disable', 'help'):
+                await ctx.reply('Cannot enable this command.')
+            else:
+                db['Enabled'][str(ctx.guild.id)][command.lower()] = True
+                await ctx.reply(f'Command "{command.lower()}" enabled.')
+        else:
+            await ctx.reply(f'Command "{command.lower()}" not found.')
+    
+    @commands.command()
+    @commands.has_guild_permissions(manage_guild = True)
+    async def disable(self, ctx, command):
+        if command.lower() in (command.name for command in ctx.bot.commands):
+            if command.lower() in ('enable', 'disable', 'help'):
+                await ctx.reply(f'Cannot disable this command.')
+            else:
+                db['Enabled'][str(ctx.guild.id)][command.lower()] = False
+                await ctx.reply(f'Command "{command.lower()}" disabled.')
+        else:
+            await ctx.reply(f'Command "{command.lower()}" not found.')
+    '''
+    
+    @commands.command()
+    @commands.has_permissions(manage_messages = True)
+    @commands.bot_has_permissions(manage_messages = True, read_message_history = True)
+    async def purge(self, ctx, limit: int):
+        await ctx.message.delete()
+        purge = await ctx.channel.purge(limit = limit)
+        await ctx.send(f'Purged {len(purge)} message{str() if len(purge) == 1 else "s"}.', delete_after = 3)
+    
+    @commands.command(aliases = ['mute'])
+    @commands.has_permissions(moderate_members = True)
+    @commands.bot_has_permissions(moderate_members = True)
+    async def timeout(self, ctx, member: discord.Member, seconds: int = None):
+        if seconds == None:
+            await member.timeout(None, reason = f'By {ctx.author}')
+            await ctx.reply(f"`{member}`'s timeout has been removed.")
+        else:
+            await member.timeout(datetime.timedelta(seconds = seconds), reason = f'By {ctx.author}')
+            await ctx.reply(f'`{member}` is timed out until {discord.utils.format_dt(datetime.datetime.today() + datetime.timedelta(seconds = seconds), "D")}.')
+
+async def setup(bot):
+    await bot.add_cog(Moderation())
