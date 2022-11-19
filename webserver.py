19bd06e17120bdcf79474901821f0d8a94baebbe
Senjienji <83118934+Senjienji@users.noreply.github.com>
2022-11-19 16:10:01 +0800

    Update webserver.py
    removed """invalid characters""" pt. 2

--- webserver.py
+++ webserver.py
@@ -1,32 +1,32 @@
-import flask
-from threading import Thread
-import discord
+from flask import Flask, redirect
+from threading import Thread
+import discord
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return """
-<!doctype html>
+def home():
+    return """
+<!doctype html>
 <html>
-    <head>
-        <title>Senjibot</title>
-    </head>
-    <body>
+    <head>
+        <title>Senjibot</title>
+    </head>
+    <body>
         <h1>WIP</h1>
-    </body>
+    </body>
 </html>
     """
 
 @app.route("/invite")
-def invite():
-    return flask.redirect(discord.utils.oauth_url(929244105320570931, permissions = discord.Permissions(
-        read_messages = True,
-        send_messages = True,
-        manage_messages = True,
+def invite():
+    return redirect(discord.utils.oauth_url(929244105320570931, permissions = discord.Permissions(
+        read_messages = True,
+        send_messages = True,
+        manage_messages = True,
         embed_links = True
-    )))
+    )))
 
-def host():
-    t = threading.Thread(target = lambda: app.run(host = "0.0.0.0", port = 8080))
-    t.start()
+def host():
+    t = Thread(target = lambda: app.run(host = "0.0.0.0", port = 8080))
+    t.start()
