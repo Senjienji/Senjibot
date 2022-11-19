bb36f85713b635bc1c0e2f4b097e7eaec78539cb
Senjienji <83118934+Senjienji@users.noreply.github.com>
2022-11-19 16:22:13 +0800

    Update main.py
    removed website stuff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands
-from webserver import host
 import pymongo
 import random
 import time
@@ -160,5 +159,4 @@ async def execute(ctx, *, content):
 async def leave(ctx, *, guild: discord.Guild):
     await guild.leave()
 
-host()
 bot.run(os.environ["DISCORD_TOKEN"])
