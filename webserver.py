7c38de026b48f0eee75bb0d6dae48a027bf35390
Senjienji <83118934+Senjienji@users.noreply.github.com>
2022-11-19 16:02:16 +0800

    Create webserver.py

---
+++ webserver.py
@@ -0,0 +1,32 @@
+import flask 
+from threading import Thread
+import discord
+
+app = flask.Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Senjibot</title>
+    </head>
+    <body>
+        <h1>WIP</h1>
+    </body>
+</html>
+    """
+
+@app.route("/invite")
+def invite():
+    return flask.redirect(discord.utils.oauth_url(929244105320570931, permissions = discord.Permissions(
+        read_messages = True,
+        send_messages = True,
+        manage_messages = True,
+        embed_links = True
+    )))
+
+def keep_alive():
+    t = threading.Thread(target = lambda: app.run(host = "0.0.0.0", port = 8080))
+    t.start()
