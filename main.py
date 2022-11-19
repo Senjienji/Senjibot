238898f8a07641da84602d2e4db0f638720e344c
Senjienji <83118934+Senjienji@users.noreply.github.com>
2022-11-19 16:53:19 +0800

    Update main.py
    added guilds intent

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ bot = commands.Bot(
         roles = False,
         replied_user = False
     ), intents = discord.Intents(
+        guilds = True,
         members = True,
         messages = True,
         message_content = True
