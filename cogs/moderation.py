fc4d26a6f68cdba1e2599d6339b9b31f2c053da9
Senjienji <83118934+Senjienji@users.noreply.github.com>
2022-12-06 14:35:44 +0800

    bug fixing pt.3

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -72,6 +72,7 @@ class Moderation(commands.Cog):
     
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
+        print(f'{payload.emoji = }')
         guild = self.bot.get_guild(payload.guild_id)
         if guild == None: return
         
@@ -90,6 +91,8 @@ class Moderation(commands.Cog):
                     await member.add_roles(role)
                     message = await guild.get_channel(payload.channel_id).fetch_message(payload.message_id)
                     for reaction in message.reactions:
+                        print(f'{reaction.emoji = }')
+                        print(f'{reaction.emoji == payload.emoji}')
                         if reaction.emoji != payload.emoji and str(reaction.emoji) in rr[str(payload.message_id)]:
                             async for user in reaction.users():
                                 if user == member:
