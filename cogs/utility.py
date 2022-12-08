0d2a1d1af476f00f29a1047ce83fe934efa4b54f
Senjienji <83118934+Senjienji@users.noreply.github.com>
2022-12-08 20:20:07 +0800

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -1,7 +1,15 @@
 import discord
 from discord.ext import commands
-import random
-import time
+import datetime
+import pymongo
+import os
+
+client = pymongo.MongoClient(
+    f'mongodb+srv://Senjienji:{os.environ["PASSWORD"]}@senjienji.czypcav.mongodb.net/?retryWrites=true&w=majority',
+    server_api = pymongo.server_api.ServerApi('1'),
+)
+db = client.senjibot
+snipe_col = db.snipe
 
 class Utility(commands.Cog):
     @commands.command()
@@ -53,6 +61,60 @@ class Utility(commands.Cog):
         if ctx.message.attachments != []:
             embed.set_image(url = ctx.message.attachments[0].url)
         await message.edit(embed = embed)
+    
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        if message.guild.id == None: return
+        
+        doc = snipe_col.find_one({'guild': message.guild.id})
+        if doc == None:
+            doc = {
+                'guild': message.guild.id,
+                'channels': {} #{channel.id: {}}
+            }
+            snipe_col.insert_one(doc)
+        doc['channels'][str(message.channel.id)] = {
+            'attachment': None if message.attachments == [] else message.attachments[0].proxy_url,
+            'content': message.content,
+            'author': message.author.id,
+            'created_at': message.created_at.timestamp(),
+            'reference': message.reference.message_id if message.reference else None
+        }
+        snipe_col.update_one(
+            {'_id': doc['_id']},
+            {'$set': {'channels': doc['channels']}}
+        )
+    
+    @commands.command()
+    async def snipe(self, ctx, channel: discord.TextChannel = None):
+        if channel == None:
+            channel = ctx.channel
+        doc = snipe_col.find_one({'guild': ctx.guild.id})
+        if doc == None or str(channel.id) not in doc['channels']:
+            await ctx.reply('Nothing found.')
+        else:
+            fields = doc['channels'][str(channel.id)]
+            author = ctx.guild.get_member(fields['author'])
+            created_at = datetime.datetime.fromtimestamp(fields['created_at'])
+            try:
+                reference = await channel.fetch_message(fields['reference'])
+            except (discord.NotFound, discord.Forbidden):
+                reference = None
+            
+            embed = discord.Embed(
+                description = fields['content'],
+                color = 0xffe5ce,
+                timestamp = created_at
+            )
+            if reference:
+                embed.description = f'> {(chr(10) + ">").join(reference.content.split())}{chr(10)}{reference.author.mention} ' + embed.description
+            if fields['attachment']:
+                embed.set_image(url = fields['attachment'])
+            embed.set_author(
+                name = str(author),
+                icon_url = author.display_avatar.url
+            )
+            await ctx.reply(embed = embed) 
 
 async def setup(bot): 
     await bot.add_cog(Utility())
