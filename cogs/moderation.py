28e96f983a0eb56ffaeb59cdb11382b56c8f415c
Senjienji <83118934+Senjienji@users.noreply.github.com>
2022-12-10 21:15:42 +0800

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,6 +1,7 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
-import datetime
+from typing import *
 import pymongo
 import os
 
@@ -10,65 +11,20 @@ client = pymongo.MongoClient(
 )
 db = client.senjibot
 rr_col = db.reaction_roles
-prefix_col = db.prefix
 
 class Moderation(commands.Cog):
     def __init__(self, bot: commands.Bot):
         super().__init__()
         self.bot = bot
     
-    @commands.command()
-    @commands.has_guild_permissions(kick_members = True)
-    @commands.bot_has_guild_permissions(kick_members = True)
-    async def kick(self, ctx, member: discord.Member, *, reason = 'no reason'):
-        await ctx.guild.kick(member, reason = f'By {ctx.author} for {reason}.')
-        await ctx.reply(f'`{member}` kicked for {reason}.')
-    
-    @commands.command()
-    @commands.has_guild_permissions(ban_members = True)
-    @commands.bot_has_guild_permissions(ban_members = True)
-    async def ban(self, ctx, user: discord.User, *, reason = 'no reason'):
-        await ctx.guild.ban(user, reason = f'By {ctx.author} for {reason}', delete_message_days = 0)
-        await ctx.reply(f'`{user}` banned for {reason}.')
-    
-    @commands.command()
-    @commands.has_guild_permissions(ban_members = True)
-    @commands.bot_has_guild_permissions(ban_members = True)
-    async def unban(self, ctx, *, user: discord.User):
-        await ctx.guild.unban(user)
-        await ctx.reply(f'`{user}` unbanned.')
-    
-    @commands.command()
-    @commands.has_permissions(manage_messages = True)
-    @commands.bot_has_permissions(manage_messages = True, read_message_history = True)
-    async def purge(self, ctx, limit: int):
-        await ctx.message.delete()
-        purge = await ctx.channel.purge(limit = limit)
-        await ctx.send(f'Purged {len(purge)} message{str() if len(purge) == 1 else "s"}.', delete_after = 3)
-    
-    @commands.command(aliases = ['mute'])
-    @commands.has_permissions(moderate_members = True)
-    @commands.bot_has_permissions(moderate_members = True)
-    async def timeout(self, ctx, member: discord.Member, seconds: int = None):
-        if seconds == None:
-            await member.timeout(None, reason = f'By {ctx.author}')
-            await ctx.reply(f"`{member}`'s timeout has been removed.")
-        else:
-            await member.timeout(datetime.timedelta(seconds = seconds), reason = f'By {ctx.author}')
-            await ctx.reply(f'`{member}` is timed out until {discord.utils.format_dt(datetime.datetime.today() + datetime.timedelta(seconds = seconds), "D")}.')
-    
-    @commands.command()
-    async def prefix(self, ctx, prefix = None):
-        if prefix == None:
-            await ctx.reply(f'My current prefix is `{ctx.bot.command_prefix(ctx.bot, ctx.message)}`.')
-        elif ctx.author.guild_permissions.manage_guild:
-            prefix_col.find_one_and_update(
-                {'guild': ctx.guild.id},
-                {'$set': {'prefix': prefix}}
-            )
-            await ctx.reply(f'Prefix has been changed to `{ctx.bot.command_prefix(ctx.bot, ctx.message)}`.')
-        else:
-            raise commands.MissingPermissions(['manage_guild'])
+    @app_commands.command(description = 'Purge messages')
+    @app_commands.describe(limit = 'The amount of messages to delete')
+    @app_commands.guild_only()
+    @app_commands.checks.has_permissions(manage_messages = True)
+    @app_commands.checks.bot_has_permissions(manage_messages = True, read_message_history = True)
+    async def purge(self, inter, limit: int):
+        purge = await inter.channel.purge(limit = limit)
+        await inter.response.send_message(f'Purged {len(purge)} message{"s" if len(purge) > 1 else None}.', ephemeral = True)
     
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
@@ -153,6 +109,7 @@ class Moderation(commands.Cog):
                 {'$set': {'reaction_roles': rr}}
             )
     
+    reaction_role = 
     @commands.group(
         aliases = ['rr'],
         help = '''Type 0: Normal
